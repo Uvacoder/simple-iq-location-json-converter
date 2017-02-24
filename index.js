bf2cf992218ab150b0b3b1957d7cba69d742a0e5
Samantha Ming <samantham@iqmetrix.com>
2017-02-24 13:33:01 -0800

    adjust style

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var sampleLocations = [{ "LocationType": null, "LocationSubType": null, "Address": { "AddressLine1": "9915-108A Avenue", "AddressLine2": null, "City": "Edmonton", "StateCode": "AB", "StateName": "Alberta", "CountryCode": "CA", "CountryName": "Canada", "Zip": "T5H4L6" }, "Contacts": [], "StorePhoneNumbers": [{ "Description": null, "Number": "7804460866", "Extension": null }], "Area": null, "StoreHours": { "Monday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Tuesday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Wednesday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Thursday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Friday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Saturday": null, "Sunday": null }, "Geography": null, "TimeZone": null, "Id": 45356, "Name": "AirTouch Communications Ltd", "DisplayName": "", "Description": "", "Roles": [{ "Name": "Location" }], "Role": "Location", "SortName": "airtouch communications ltd", "Attributes": { "Abbreviation": "ATCDT" }, "Relationships": [{ "Id": 70535, "Definition": 11, "Source": 56876, "Destination": 45356, "CreatedUtc": "2015-06-09T10:19:08.539Z", "Version": 1 }], "Version": 427, "CreatedUtc": "2015-05-06T15:25:44.732Z", "LastModifiedUtc": "2017-02-22T09:30:44.101Z", "CorrelationId": "2", "ClientEntityId": null, "TypeId": 4122, "Logo": null }, { "LocationType": null, "LocationSubType": null, "Address": { "AddressLine1": "9915-108A Ave.", "AddressLine2": null, "City": "Edmonton", "StateCode": "AB", "StateName": "Alberta", "CountryCode": "CA", "CountryName": "Canada", "Zip": "T5H4L6" }, "Contacts": [], "StorePhoneNumbers": [{ "Description": null, "Number": "7804252355", "Extension": null }], "Area": null, "StoreHours": { "Monday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Tuesday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Wednesday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Thursday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Friday": { "Open": { "Hour": 8, "Minute": 0 }, "Close": { "Hour": 17, "Minute": 0 } }, "Saturday": null, "Sunday": null }, "Geography": null, "TimeZone": null, "Id": 45355, "Name": "Head Office", "DisplayName": "", "Description": "", "Roles": [{ "Name": "Location" }], "Role": "Location", "SortName": "head office", "Attributes": { "Abbreviation": "AMLDT" }, "Relationships": [{ "Id": 70534, "Definition": 11, "Source": 56876, "Destination": 45355, "CreatedUtc": "2015-06-09T10:19:05.816Z", "Version": 1 }], "Version": 437, "CreatedUtc": "2015-05-06T15:25:44.069Z", "LastModifiedUtc": "2017-02-22T09:30:43.397Z", "CorrelationId": "1", "ClientEntityId": null, "TypeId": 4122, "Logo": null }];
+
 $(document).ready(function () {
 
   $('#json-form').on('submit', (e) => {
@@ -15,6 +17,16 @@ $(document).ready(function () {
       generateTable(JSON.parse(value));
     }
 
+  });
+
+  $('#clear-form').on('click', (e) => {
+    e.preventDefault();
+    $('#json-textarea').val('');
+  });
+
+  $('#load-sample').on('click', (e) => {
+    e.preventDefault();
+    $('#json-textarea').val(JSON.stringify(sampleLocations));
   });
 
 });
@@ -36,9 +48,7 @@ function fetchLocation(locations, categories) {
         prev[cat] = `${AddressLine1}, ${City} ${StateName} ${CountryName} ${Zip}`;
       }
       else if (cat === 'StorePhoneNumbers') {
-        if (location.StorePhoneNumbers[0]) {
-          prev[cat] = `${location.StorePhoneNumbers[0].Number}`;
-        }
+        prev[cat] = location.StorePhoneNumbers[0] ? `${location.StorePhoneNumbers[0].Number}` : '';
       }
       else {
         prev[cat] = location[cat];
@@ -53,25 +63,12 @@ function generateTable(locations) {
   let categories = defaultCategories();
   let locs = fetchLocation(locations);
 
-  let header = categories.reduce((prev, cur) => {
-    if (cur === 'Id') {
-      prev += `<th>LocationId</th>`;
-    } else {
-      prev += `<th>${cur}</th>`;
-    }
-    return prev;
-  }, '');
-  $('#csv-test').html(`<thead><tr>${header}</tr></thead>`);
-
-  let body = '';
-  for (let loc of locs) {
-    body += '<tr>';
-    for (let key in loc) {
-      body += `<td>${loc[key]}</td>`;
-    }
-    body += '</tr>';
-  }
-  $('#csv-test').append(`<tbody>${body}</tbody>`);
+  let template = $('#csv-table-template').html();
+  let compiled = _.template(template)({
+    categories,
+    locs
+  });
+  $('#csv-table').html(compiled);
 }
 
 function selectElementContents(el) {
